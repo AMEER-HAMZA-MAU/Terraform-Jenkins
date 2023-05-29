a16ed5650b3ae5c706ed1083bb84d57f6feff734
AMEER HAMZA <114941881+AMEER-HAMZA-MAU@users.noreply.github.com>
2023-05-29 12:05:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-01107263728f3bef4" # us-west-2
+  ami           = "ami-05842f1afbf311a43" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
